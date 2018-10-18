50a88c42e75f6e57f3c073c16d1652066efef5e3
cjh5414 <cjh5414@gmail.com>
2018-10-18 22:47:15 +0900

    Correct Stack and Queue 6 solution

--- StackAndQueue-6/src/Solution.java
+++ StackAndQueue-6/src/Solution.java
@@ -6,14 +6,11 @@ class Solution {
         int[] answer = new int[prices.length];
 
         for (int i = 0; i < prices.length - 1; i++) {
-            int count = 1;
             for (int j = i + 1; j < prices.length; j++) {
                 if (j == prices.length - 1 || prices[i] > prices[j]) {
-                    answer[i] = count;
+                    answer[i] = j - i;
                     break;
                 }
-                else
-                    count++;
             }
         }
 
