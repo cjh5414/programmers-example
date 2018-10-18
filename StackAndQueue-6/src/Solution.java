d7e086aadb4e01ab96dcd8fd73b5bb5a012d4f4a
cjh5414 <cjh5414@gmail.com>
2018-10-18 22:39:04 +0900

    Modify Statck and Queue 6 solution

--- StackAndQueue-6/src/Solution.java
+++ StackAndQueue-6/src/Solution.java
@@ -3,7 +3,20 @@
  */
 class Solution {
     public int[] solution(int[] prices) {
-        int[] answer = {};
+        int[] answer = new int[prices.length];
+
+        for (int i = 0; i < prices.length - 1; i++) {
+            int count = 1;
+            for (int j = i + 1; j < prices.length; j++) {
+                if (j == prices.length - 1 || prices[i] >= prices[j]) {
+                    answer[i] = count;
+                    break;
+                }
+                else
+                    count++;
+            }
+        }
+
         return answer;
     }
 }
