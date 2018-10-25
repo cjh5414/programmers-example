948da0a740feb6acb1d180bfeefbe373f5be1699
cjh5414 <cjh5414@gmail.com>
2018-10-25 14:29:38 +0900

    Correct Heap 2 solution

--- Heap-2/src/Solution.java
+++ Heap-2/src/Solution.java
@@ -1,19 +1,34 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 /**
  * Created by jihun on 2018. 10. 24..
  */
 class Solution {
     public int solution(int stock, int[] dates, int[] supplies, int k) {
-        int answer = 0;
-
-        for (int i = 0; i < dates.length; i++) {
-            if (stock <= dates[i]) {
-                stock += supplies[i];
-                answer++;
+        PriorityQueue<Integer> q = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2 - o1;
             }
-            if (stock >= k) return answer;
+        });
+
+        int count = 0;
+        int date_idx = 0;
+        for (int i = 1; i <= k; i++) {
+            if (stock == 0) {
+                stock += q.remove();
+                count++;
+            }
+
+            if (date_idx < dates.length && i == dates[date_idx]) {
+                q.add(supplies[date_idx]);
+                date_idx++;
+            }
+
+            stock--;
         }
 
-        if (stock < k) answer++;
-        return answer;
+        return count;
     }
 }
